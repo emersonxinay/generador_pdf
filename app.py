b0098ccf96def838656f046754f6fdec2691356d
emerson <xinayespinoza@gmail.com>
2023-10-28 00:06:18 -0300

    fix generador

--- app.py
+++ app.py
@@ -3,10 +3,40 @@ import tempfile
 from PIL import Image
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
-import io
-from flask import Flask, request, render_template, send_file
+from flask import Flask, request, render_template, send_file, url_for
 
 app = Flask(__name__)
+
+# Función para procesar imágenes y generar el PDF
+def generar_pdf(imagenes):
+    pdf_output = tempfile.mktemp(suffix=".pdf")
+    c = canvas.Canvas(pdf_output, pagesize=letter)
+
+    ancho_pagina, alto_pagina = letter
+
+    try:
+        for idx, imagen in enumerate(imagenes):
+            # Procesar la imagen y guardarla temporalmente
+            temp_image_path = tempfile.mktemp(suffix=".png")
+            imagen.save(temp_image_path)
+
+            # Obtener el tamaño de la imagen después de la posible compresión
+            ancho_imagen, alto_imagen = imagen.size
+            x = (ancho_pagina - ancho_imagen) / 2
+            y = (alto_pagina - alto_imagen) / 2
+
+            # Cargar la imagen en el canvas del PDF
+            c.drawImage(temp_image_path, x, y, width=ancho_imagen, height=alto_imagen)
+            c.showPage()
+
+            # Eliminar la imagen temporal después de procesarla
+            os.remove(temp_image_path)
+
+        c.save()
+        return pdf_output  # Devolver la ruta del archivo PDF
+    except Exception as e:
+        print("Error al generar el PDF:", e)
+        return None
 
 @app.route('/')
 def index():
@@ -14,47 +44,27 @@ def index():
 
 @app.route('/generate_pdf', methods=['POST'])
 def generate_pdf():
-    uploaded_files = request.files.getlist("file[]")
-    pdf_output = io.BytesIO()
-    c = canvas.Canvas(pdf_output, pagesize=letter)
+    try:
+        # Obtener las imágenes desde la solicitud (request)
+        imagenes = []
+        for uploaded_file in request.files.getlist("file[]"):
+            image = Image.open(uploaded_file)
+            imagenes.append(image)
 
-    ancho_pagina, alto_pagina = letter
-    tamanio_maximo = 2 * 1024 * 1024  # 2 MB
+        # Generar el PDF
+        pdf_path = generar_pdf(imagenes)
 
-    for uploaded_file in uploaded_files:
-        try:
-            image_data = uploaded_file.read()  # Leer datos binarios de la imagen
-            image = Image.open(io.BytesIO(image_data))  # Abrir la imagen usando Pillow
+        if pdf_path:
+            # Obtener la URL del PDF temporal usando url_for
+            pdf_url = url_for('static', filename=os.path.basename(pdf_path))
 
-            # Comprimir la imagen si es necesario
-            if len(image_data) > tamanio_maximo:
-                image.thumbnail((ancho_pagina, alto_pagina))
-
-            # Obtener el tamaño de la imagen después de la posible compresión
-            ancho_imagen, alto_imagen = image.size
-
-            # Calcular la posición para centrar la imagen en la página
-            x = (ancho_pagina - ancho_imagen) / 2
-            y = (alto_pagina - alto_imagen) / 2
-
-            # Cargar la imagen en el canvas del PDF
-            c.drawImage(image, x, y, width=ancho_imagen, height=alto_imagen)
-            c.showPage()
-
-        except Exception as e:
-            print("Error processing image:", e)
-            continue
-
-    c.save()
-    pdf_output.seek(0)
-
-    # Crear un archivo temporal seguro para el PDF
-    with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as temp_file:
-        temp_filename = temp_file.name
-        temp_file.write(pdf_output.getvalue())
-
-    # Enviar el archivo PDF como respuesta en línea y eliminar el archivo temporal después de ser servido
-    return send_file(temp_filename, as_attachment=False, download_name='resultado.pdf', mimetype='application/pdf')
+            # Enviar la URL del PDF como respuesta
+            return render_template('index.html', pdf_url=pdf_url)
+        else:
+            return "Error al generar el PDF", 500
+    except Exception as e:
+        print("Error:", e)
+        return "Error al procesar las imágenes", 500
 
 if __name__ == '__main__':
     app.run(debug=True)
